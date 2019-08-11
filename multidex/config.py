18518a162a508f6dd79339ee790f002ed116c44c
Vasilii Makarov <drmoriarty.0@gmail.com>
2019-08-11 13:01:46 +0300

    fix config

--- multidex/config.py
+++ multidex/config.py
@@ -1,11 +1,13 @@
-def can_build(plat):
-	return plat=="android"
+def can_build(env, platform):
+	if platform == "android":
+		return True
+	return False
 
 def configure(env):
 	if (env['platform'] == 'android'):
-		#env.android_add_java_dir("android/src/")
 		env.android_add_to_attributes("android/AndroidManifestAttributeChunk.xml")
 		env.android_add_dependency("compile 'com.android.support:multidex:1.0.1'")
 		env.android_add_default_config("multiDexEnabled true")
+		#env.android_add_default_config("applicationId 'free.games.match.three.puzzle.galactic.adventure_2.android'")
 		env.disable_module()
 
