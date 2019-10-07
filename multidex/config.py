d28ec3bdce297638300dc32b0e662c916ff9eeca
Vasiliy Makarov <drmoriarty.0@gmail.com>
2019-10-08 00:09:27 +0300

    use androidx

--- multidex/config.py
+++ multidex/config.py
@@ -6,8 +6,7 @@ def can_build(env, platform):
 def configure(env):
 	if (env['platform'] == 'android'):
 		env.android_add_to_attributes("android/AndroidManifestAttributeChunk.xml")
-		env.android_add_dependency("compile 'com.android.support:multidex:1.0.1'")
+		env.android_add_dependency("implementation 'androidx.multidex:multidex:2.0.0'")
 		env.android_add_default_config("multiDexEnabled true")
-		#env.android_add_default_config("applicationId 'free.games.match.three.puzzle.galactic.adventure_2.android'")
 		env.disable_module()
 
